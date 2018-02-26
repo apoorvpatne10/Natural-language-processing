97464060256060bdcbdcbe7022fc15c967441a01
Apoorv Patne <31184004+apoorvpatne10@users.noreply.github.com>
2018-02-27 00:23:05 +0530

    Update natural_language_processing.py

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -13,20 +13,12 @@ from nltk.stem.porter import PorterStemmer
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
-# Cleaning the dataset
-
-# initiaalizing an empty list which will contain all the 1000 cleaned reviews
 corpus = []
 for i in range(0, 1000):
-    # Only including words consisting of english letters
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
-    # Converting each word from lower case to upper case
     review = review.lower()
-    # Splitting the review into list of words
     review = review.split()
     ps = PorterStemmer()
-    # Eliminating the stopwords and apply stemming on every word to
-    # reduce it to its simplest form
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)    
     corpus.append(review)
@@ -38,14 +30,59 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 
-# Naive Bayes - classification
+# Logistic regression 
+#Splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#Fitting logistic regression to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+#Predicting the test set results
+y_pred = classifier.predict(X_test)
+
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+"""
+# KNN Classifier
+#Splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#Fitting classifier the training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski' , p = 2)
+classifier.fit(X_train, y_train)
+
+#Predicting the test set results
+y_pred = classifier.predict(X_test)
+
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+"""
+
+
+"""
+# SVM classification
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Fitting classifier to the training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'sigmoid', gamma = 0.0008, random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -54,16 +91,17 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+"""
 
 """
-# Random forest classification
+# Naive Bayes - classification
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Fitting classifier to the Training set
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy', random_state = 0)
+# Fitting classifier to the training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -93,22 +131,16 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 """
-"""
-# SVM classification
 
+"""
+# Random forest classification
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Fitting SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'sigmoid', gamma = 0.0008, random_state = 0)
+# Fitting classifier to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -118,43 +150,6 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 """
-"""
-# Logistic regression 
-#Splitting the dataset into training set and test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-
-#Fitting logistic regression to the training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
-
-#Predicting the test set results
-y_pred = classifier.predict(X_test)
-
-#Making the confusion matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-"""
-
-
-# KNN Classifier
-#Splitting the dataset into training set and test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-
-#Fitting classifier the training set
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski' , p = 2)
-classifier.fit(X_train, y_train)
-
-#Predicting the test set results
-y_pred = classifier.predict(X_test)
-
-#Making the confusion matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
 
 """
 # Kernel SVM
@@ -174,6 +169,3 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 """
- 
-
-##
